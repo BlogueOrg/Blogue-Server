bb5508a9ce01a132ffdf902079bb8dd8e71dbf16
Jimin Kim <0305jimin@gmail.com>
2024-05-23 04:17:58 +0100

    Fix: making swagger ui accessible by modifying spring security configs

---
+++ src/main/java/com/blogue/blogue/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.blogue.blogue.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/user/**").authenticated()
+                .requestMatchers("/manager/**").hasAnyRole("ADMIN", "MANAGER")
+                .requestMatchers("/admin/**").hasAnyRole("ADMIN")
+                .anyRequest().permitAll());
+
+        return http.build();
+    }
+}
