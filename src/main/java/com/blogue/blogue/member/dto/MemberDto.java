25342d54730f1b16e657ae62c776d4aa871a82de
Jimin Kim <0305jimin@gmail.com>
2024-05-25 14:22:55 +0200

    Feat: adding MemberDTO class

---
+++ src/main/java/com/blogue/blogue/member/dto/MemberDto.java
@@ -0,0 +1,15 @@
+package com.blogue.blogue.member.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+import java.util.stream.Collector;
+
+@Data
+@AllArgsConstructor
+public class MemberDto {
+    private String name;
+
+}
