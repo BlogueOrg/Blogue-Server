10bf4670c0ae49d9166d714b4987a13ecc64a368
Jimin Kim <0305jimin@gmail.com>
2024-05-25 16:17:41 +0200

    Feat: adding UpdateMemberUsernameResponse, Request DTO classes

---
+++ src/main/java/com/blogue/blogue/member/dto/request/UpdateMemberUsernameRequest.java
@@ -0,0 +1,9 @@
+package com.blogue.blogue.member.dto.request;
+
+
+import lombok.Data;
+
+@Data
+public class UpdateMemberUsernameRequest {
+    private String username;
+}
---
+++ src/main/java/com/blogue/blogue/member/dto/response/UpdateMemberUsernameResponse.java
@@ -0,0 +1,11 @@
+package com.blogue.blogue.member.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UpdateMemberUsernameResponse {
+    private Long id;
+    private String username;
+}
