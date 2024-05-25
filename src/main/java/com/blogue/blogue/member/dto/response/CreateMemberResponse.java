199cd50a060c762f5d026b8899ec8745749f55e8
Jimin Kim <0305jimin@gmail.com>
2024-05-25 15:49:44 +0200

    Refactor: (CreateMemberResponse) using `@AllArgsConstructor`

--- src/main/java/com/blogue/blogue/member/dto/response/CreateMemberResponse.java
+++ src/main/java/com/blogue/blogue/member/dto/response/CreateMemberResponse.java
@@ -1,12 +1,11 @@
 package com.blogue.blogue.member.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@AllArgsConstructor
 public class CreateMemberResponse {
     private Long id;
-
-    public CreateMemberResponse(Long id) {
-        this.id = id;
-    }
 }
