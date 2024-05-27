d093811e7e96003c2eae9f7a8eff6a738b9b1cfb
Jimin Kim <0305jimin@gmail.com>
2024-05-27 16:06:36 +0200

    Feat: responding statusCode value not only message and the data

--- src/main/java/com/blogue/blogue/util/ResponseDTO.java
+++ src/main/java/com/blogue/blogue/util/ResponseDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class ResponseDTO <T> {
-    private ResponseMessage message;
+    private Integer statusCode;
+    private String statusMessage;
     private T data;
 }
