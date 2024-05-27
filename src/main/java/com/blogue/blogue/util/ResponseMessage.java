ea126182d76e3af2b0364323a194b79038d01b85
Jimin Kim <0305jimin@gmail.com>
2024-05-27 15:52:17 +0200

    Feat: ResponseMessage

---
+++ src/main/java/com/blogue/blogue/util/ResponseMessage.java
@@ -0,0 +1,13 @@
+package com.blogue.blogue.util;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum ResponseMessage {
+
+    MEMBER_CREATED("회원 생성이 완료되었습니다.");
+
+    private String message;
+
+
+}
