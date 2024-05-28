0e7aca2a6d47f9a03a5189d13356d64895f0c9d4
Jimin Kim <0305jimin@gmail.com>
2024-05-28 18:20:04 +0200

    Fix: `org.springframework.web.HttpMediaTypeNotAcceptableException`
    `: No acceptable representation` by adding build() and renaming the variable from data to body

--- src/main/java/com/blogue/blogue/util/ResponseDTO.java
+++ src/main/java/com/blogue/blogue/util/ResponseDTO.java
@@ -1,29 +1,34 @@
 package com.blogue.blogue.util;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.net.http.HttpResponse;
+
 @Data
 @Builder
 public class ResponseDTO <T> {
     private Integer statusCode;
     private String statusMessage;
-    private T data;
+    @JsonIgnore
     private Status status;
+    private T body;
 
-    public ResponseDTO(Status status, T data) {
+
+    public ResponseDTO(Status status, T body) {
         this.status = status;
-        this.data = data;
+        this.body = body;
     }
 
-    public ResponseDTO(Integer statusCode, String statusMessage, T data, Status status) {
+    public ResponseDTO(Integer statusCode, String statusMessage, Status status, T body) {
         this.statusCode = statusCode;
         this.statusMessage = statusMessage;
         this.status = status;
-        this.data = data;
+        this.body = body;
     }
 
     public ResponseEntity returnResponseEntity() {
@@ -32,7 +37,8 @@ public class ResponseDTO <T> {
                 .body(ResponseDTO.builder()
                         .statusCode(status.getHttpStatus().value())
                         .statusMessage(status.getStatusMessage())
-                        .data(data)
+                        .body(body)
+                        .build()
                 );
     }
 }
